0d7ab2123d13309efc72c14789cc92c90165d444
Nathalia Adriele <nat-adriele@hotmail.com>
2025-07-25 02:45:23 +0000

    setup

--- setup.py
+++ setup.py
@@ -5,11 +5,9 @@ Setup script para o Sistema de Prevenção de Fraudes Transacionais
 from setuptools import setup, find_packages
 import os
 
-# Lê o README para a descrição longa
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# Lê os requirements
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
